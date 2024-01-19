55f0796fd0002c7a4577f7f3e763339d7a6bcb52
B0pp3d <ollikavs@gmail.com>
2024-01-19 15:36:44 +0000

    Changes

--- db/connection.js
+++ db/connection.js
@@ -9,6 +9,8 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
+const config = {}
+
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
